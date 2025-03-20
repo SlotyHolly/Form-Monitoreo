6d750761ec05258a5e3a17a622ad9284bbcb846c
SlotyHolly <matiasmusso3032@gmail.com>
2025-03-19 22:17:16 -0300

    Fix Errors

--- app/routes.py
+++ app/routes.py
@@ -80,11 +80,8 @@ def create_report():
 
         # 🔹 Asegurar que `report_date` siempre tenga un valor
         if current_user.role == "admin":
-            if report_date is None:
-                report_date = datetime.utcnow() - timedelta(hours=3)
-            else:
-                report_date = form.report_date.data  # Mantenerlo como `date`
-                report_date = datetime.combine(report_date, datetime.utcnow().time())  # Convertir a `datetime`
+            report_date = form.report_date.data  # Mantenerlo como `date`
+            report_date = datetime.combine(report_date, datetime.utcnow().time())  # Convertir a `datetime`
         else:
             report_date = datetime.utcnow() - timedelta(hours=3)  # UTC-3
 
