f12d478776e766c9b2d2d46f39c467b965014ccd
SlotyHolly <matiasmusso3032@gmail.com>
2025-03-21 13:36:48 -0300

    Update

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 from app import create_app
-from app.migrate_db import migrateDb
 
 app = create_app()
 
 if __name__ == "__main__":
-    migrateDb()
     app.run(host="0.0.0.0", port=5000, debug=True)
