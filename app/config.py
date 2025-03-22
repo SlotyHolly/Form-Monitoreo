01eb0affe07293e46fb27bd6d0fbfbeadb014cf4
SlotyHolly <matiasmusso3032@gmail.com>
2025-03-21 21:33:52 -0300

    Update .gitignore and app/config.py

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ if os.getenv("ENVIRONMENT") == "docker":
     BASE_DIR = "/app/instance"
 else:
     BASE_DIR = Path(__file__).resolve().parent.parent / "instance"
-
+    
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "nyrmcwznsrQHzCkTCdpPFzUubqZQBa")
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'reports_wazuh.sqlite')}"
